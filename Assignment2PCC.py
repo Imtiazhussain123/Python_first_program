b4a2226fb9cba0cca1124c0aabc5ce7b4c327c83
Imtiaz Hussain <imtiazh183@gmail.com>
2017-12-13 16:40:18 +0500

    Assignment 2 of SSUET of AI by Sir Anees Ahmed (5.3, 5.4, 5.5, 5.6)

---
+++ Assignment2PCC.py
@@ -0,0 +1,85 @@
+# Assignment 2 by (Sir Anees Ahmed)  SSUET AI course
+
+# ***uncomment each code by removing # using ( ctrl + / ) in pycharm editor ******
+
+# PROBLEM 5.3
+
+# #Phase 1
+# alien_colors= "green"
+# if "green" in alien_colors:
+#     print("You scored just 5 points")
+#
+# #Phase 2
+# alien_colors= "green"
+# if "blue" in alien_colors:
+#     print("You scored just 5 point")
+#
+# #################################################
+#
+# # PROBLEM 5.4
+# #Phase 1
+#
+# alien_color = "green"
+# if "green" in alien_colors:
+#     print("Yor scored 5 points!")
+# else:
+#     print("You earn 10 point!")
+#
+# #Phase 2
+#
+# alien_color = "green"
+# if "blue" in alien_colors:
+#     print("Yor scored 10 points!")
+# else:
+#     print("You earn 5 point!")
+#
+# ###################################################
+#
+# # PROBLEM 5.5
+# # Phase 1
+#
+# alien_color = "red"
+# if "green" in alien_color:
+#     print("you scored 5 points")
+# elif "yellow" in alien_color:
+#     print("you scored 10 points")
+# else:
+#     print("you scored 15 points")
+#
+# # Phase 2
+#
+# alien_color = "green"
+# if "green" in alien_color:
+#     print("you scored 5 points")
+# elif "yellow" in alien_color:
+#     print("you scored 10 points")
+# else:
+#     print("you scored 15 points")
+#
+# # Phase 3
+#
+# alien_color = "yellow"
+# if "green" in alien_color:
+#     print("you scored 5 points")
+# elif "yellow" in alien_color:
+#     print("you scored 10 points")
+# else:
+#     print("you scored 15 points")
+#
+# #################################################
+#
+# # PROBLEM 5.6
+#
+# age= 78;
+# if age<=2:
+#     print("person is a babay")
+# elif age>2 and age<=4:
+#     print("person is toddler")
+# elif age>4 and age<=12:
+#     print("Person is a kid")
+# elif age>12 and age<=20:
+#     print("Person is a teenager")
+# elif age>20 and age<=65:
+#     print("person is a adult")
+# else:
+#     print("Person is older")
